168eedac995c7ba4bb1ec2cd7c6c8803cff61c75
hugokk10 <hugo.rosario@escola.pr.gov.br>
2026-05-06 12:30:53 +0000

    versao2

--- cardapio.js
+++ cardapio.js
@@ -1,26 +1,46 @@
 const cardapio = [
-    {nome: "X-Burguer", preco: 18.90, categoria: "lanche"},
-    {nome: "Batata-Frita", preco: 9.50, categoria: "acompanhamento"},
-    {nome: "Suco de Laranja", preco: 7.00, categoria: "bebida"},
-    {nome: "Milkshake", preco: 12.00, categoria: "sobremesa"}
-];
+  {nome: "x- burguer", preco: 18.90, catetegoria:"lanche"},
+  {nome: "batata-frita", preco: 9.50, categoria: "acompanhamneto"},
+  {nome: "sprite", preco: 4.50, categoria: "bebida"},
+  {nome: "milkshake", preco: 12.00, categoria: "sobremesa"}
+]
 
-//console.log(cardapio); //Impressão dos valores informados
-
-const pedido = [];
-
-console.log(cardapio[0].nome);
-console.log(cardapio[1].nome); //preço da batata
-console.log(cardapio[2].categoria); //categoria do suco de laranja
-
-function exibirCardapio(lista){
-    console.log("===CARDAPIO===");
-    console.log(`1. ${lista[0]}.nome} - R$ ${lista[0].preço}`)
-    console.log(`1. ${lista[1]}.nome} - R$ ${lista[1].preço}`)
-    console.log(`1. ${lista[2]}.nome} - R$ ${lista[2].preço}`)
-    console.log(`1. ${lista[3]}.nome} - R$ ${lista[3].preço}`)
+const cliente = {
+    nome: "ana",
+    fidelidade: true
 }
 
-function calcularTotal(listaPedido) (
-    let total = listaPedido
-)
+//console.log(cardapio[0].nome);
+//console.log(cardapio[1].preco);
+//console.log(cardapio[2].categoria);
+
+function exibirCardapio(lista) {
+  console.log("===CARDAPIO===");
+  console.log(`1. ${lista[0].nome} - R$ ${lista[0].cartegoria}`)
+  console.log(`2. ${lista[1].nome} - R$ ${lista[1].cartegoria}`)
+  console.log(`3. ${lista[2].nome} - R$ ${lista[2].cartegoria}`)
+  console.log(`4. ${lista[3]}.nome} - R$ ${lista[3].categoria}`)
+}
+
+function calcularTotal(listaPedido) {
+  let total = listaPedido[0].preco + listaPedido[1].preco;
+  return total;
+}
+
+function calcurComDesconto(total, clienteObj) {
+    if (clienteObj.fidelidade === true) {
+        let desconto = total * 0.10;
+        console.log('Desconto fidelidade: R${desconto}');
+        return total;
+    }
+}
+
+function exibirPedido(listaPedido){
+  console.log("===SEU PEDIDO ===");
+  console.log(`1. ${listaPedido[0].nome} - R$ ${listaPedido[0].preco}`)
+  console.log(`2. ${listaPedido[1].nome} - R$ ${listaPedido[1].preco}`)
+  console.log(`TOTAL: R$${calcularTotal(listaPedido)}`)
+}
+
+exibirCardapio(cardapio)
+exibirPedido(pedido);
