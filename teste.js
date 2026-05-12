17ed124705f7036b0ea3aecc0622961facf74732
hugokk10 <hugo.rosario@escola.pr.gov.br>
2026-05-12 11:53:34 +0000

    v1

---
+++ teste.js
@@ -0,0 +1,6 @@
+const destino = "Foz do iguaçu";
+const origem = "curitiba";
+const nomeViajante = "Luiza";
+let duracao = 8;
+let transporte = "carro";
+console.log(`${nomeViajante} viajara de ${destino} por ${duracao}h de ${transporte}.`);
