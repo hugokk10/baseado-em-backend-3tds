f4678445131fb0480c96ed194721ae5e62bdc0cc
hugokk10 <hugo.rosario@escola.pr.gov.br>
2026-05-12 11:54:27 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,7 @@
+// Exemplo de declaração de 5 variáveis para um funcionário
+
+let nomeFuncionario = "João Silva"; 
+let idadeFuncionario = 30; 
+let salarioFuncionario = 4500.50; 
+let cargoFuncionario = "Analista"; 
+let estaAtivo = true;
