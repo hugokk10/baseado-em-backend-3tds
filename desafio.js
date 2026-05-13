4a1690d91c6867aaead38c99b2ffd7fb60c125e2
hugokk10 <hugo.rosario@escola.pr.gov.br>
2026-05-13 12:39:53 +0000

    v1

--- desafio.js
+++ desafio.js
@@ -5,3 +5,11 @@ let idadeFuncionario = 30;
 let salarioFuncionario = 4500.50; 
 let cargoFuncionario = "Analista"; 
 let estaAtivo = true;
+
+console.log(`O funcionário ${nomeFuncionario} tem ${idadeFuncionario} anos. Ele trabalha como ${cargoFuncionario}`);
+console.log(`O funcionário ${estaAtivo} o salario ${salarioFuncionario}`);
+
+const salarioBase = 4500.50;
+const bonusMensal = 500;
+
+console.log(`o funcionário recebe um salário base de R$ ${salarioBase}e um bônus de R$ ${bonusMensal}.`);
