7c76afc08e092477b5b57a7dc7179319a6773ed9
hugokk10 <hugo.rosario@escola.pr.gov.br>
2026-06-30 11:36:58 +0000

    versao2

---
+++ aluno2.js
@@ -0,0 +1,12 @@
+{
+    "matricula": "2026CD34",
+    "nome": "joao gabriel",
+    "idade": 18,
+    "curso": "engenharia da producao",
+    "notas": [7.5, 8.0, 6.0],
+    "bolsista": false,
+    "endereco": {
+        "cidade": "Curitiba",
+        "estado": "Paraná"
+    }
+}
